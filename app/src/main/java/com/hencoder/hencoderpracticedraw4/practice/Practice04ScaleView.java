85c38db1b125f0ba8cfd521275d180c604edcf79
Luo Guowen <luoguowen123@qq.com>
2017-10-17 16:46:48 +0800

    2017-10-17 16:46:11 完成1~9

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice04ScaleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice04ScaleView.java
@@ -17,6 +17,7 @@ public class Practice04ScaleView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    float bitmapWidth, bitmapHeight;
 
     public Practice04ScaleView(Context context) {
         super(context);
@@ -32,13 +33,22 @@ public class Practice04ScaleView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+        bitmapWidth = bitmap.getWidth();
+        bitmapHeight = bitmap.getHeight();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        canvas.save();
+        canvas.scale(1.3f, 1.3f, point1.x + bitmapWidth / 2, point1.y + bitmapHeight / 2);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+
+        canvas.save();
+        canvas.scale(0.5f, 1.5f, point1.x + bitmapWidth * 2, point1.y + bitmapHeight / 2);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice09MatrixRotateView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice09MatrixRotateView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
@@ -17,6 +18,9 @@ public class Practice09MatrixRotateView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Matrix matrix1 = new Matrix();
+    Matrix matrix2 = new Matrix();
+    float bitmapWidth, bitmapHeight;
 
     public Practice09MatrixRotateView(Context context) {
         super(context);
@@ -32,13 +36,24 @@ public class Practice09MatrixRotateView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+        bitmapWidth = bitmap.getWidth();
+        bitmapHeight = bitmap.getHeight();
+        matrix1.postRotate(180, point1.x + bitmapWidth / 2, point1.y + bitmapHeight / 2);
+        matrix2.postRotate(45, point2.x + bitmapWidth / 2, point2.y + bitmapHeight / 2);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        canvas.setMatrix(matrix1);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.setMatrix(matrix2);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
